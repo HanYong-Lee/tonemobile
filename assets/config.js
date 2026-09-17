59c0fa778ea29444ee3504225eca1411c406ba65
HY_L <133779344+HanYong-Lee@users.noreply.github.com>
2026-09-17 10:45:55 +0900

    Change inventoryCsvUrl to new Google Sheets link
    Updated inventoryCsvUrl to a new Google Sheets CSV link.

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.T1_CONFIG = {
   // Google Sheets를 '웹에 게시 → CSV'로 설정한 뒤 주소를 입력하세요.
-  inventoryCsvUrl: "https://docs.google.com/spreadsheets/d/1IVfPsNtQpkuyflGXNKZyMwbagP07D7fTy0QxDemgaXs/edit?usp=sharing",
+  inventoryCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR_MPdGxEXnPAIBt7QrG33FJYWsykUPjWTRUivNJ0_J3AGU3ul0l2RLG5lU2pJ_JepFUnCqplFwNQ0X/pub?gid=0&single=true&output=csv",
   storesCsvUrl: "",
   // CTA 기록용 Google Apps Script 웹 앱 주소를 입력하세요.
   analyticsEndpoint: "https://script.google.com/macros/s/AKfycbwjE9jsOeXtzjrK5nh0L7M91h6FdrylLCbmDAKE49JWgdqQlUcY6p1E_KIxGn_sBMC6Fg/exec",
